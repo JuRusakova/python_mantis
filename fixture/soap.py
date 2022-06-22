ce33617ef6bcbb834d056fcabbb29d6d08e52875
Rusakova <wolfik2k@gmail.com>
2022-06-22 12:46:06 +0400

    25/26 задание

--- fixture/soap.py
+++ fixture/soap.py
@@ -1,7 +1,6 @@
 from suds.client import Client
 from suds import WebFault
-from fixture.project import Project
-
+from model.project import Project
 
 class SoapHelper:
 
@@ -9,7 +8,8 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client('http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl')
+        base_url = self.app.config['web']['baseUrl']
+        client = Client(base_url + "/api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
